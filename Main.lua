731010558cf439d5e0314e1e3767c4af1ba44dda
bahade2323 <bahade2323@gmail.com>
2026-07-11 21:20:04 +0800

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,6 @@
+task.spawn(function()
+	local GameId = game.GameId
+	if GameId == 10093833731 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/bahade2323/Miwa-Hub/refs/heads/main/Broken%20Blade"))()
+  end
+end)
