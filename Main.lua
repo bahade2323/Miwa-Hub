c5430e0ddfb78332b38f5f3f6a6ff1a569caf8f0
bahade2323 <bahade2323@gmail.com>
2026-07-12 20:54:22 +0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,8 @@
 task.spawn(function()
 	local GameId = game.GameId
 	if GameId == 10093833731 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/bahade2323/Miwa-Hub/refs/heads/main/Broken%20Blade"))()
+	    loadstring(game:HttpGet("https://raw.githubusercontent.com/bahade2323/Miwa-Hub/refs/heads/main/Broken%20Blade"))()
+	elseif GameId == 10267363348 then
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/bahade2323/Miwa-Hub/refs/heads/main/Broken%20Blade"))()
   end
 end)
