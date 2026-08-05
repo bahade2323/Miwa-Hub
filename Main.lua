42883b9e7d26f3a4cb4b4100e2a27cd508a5f28c
bahade2323 <bahade2323@gmail.com>
2026-08-05 13:36:42 +0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -5,6 +5,6 @@ task.spawn(function()
     elseif GameId == 10267363348 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/bahade2323/Miwa-Hub/refs/heads/main/Drain%20A%20Lake"))()
     elseif GameId == 10178802449 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/bahade2323/Miwa-Hub/refs/heads/main/%2B1%20Mine%20Per%20Click.lua"))()
+        loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/1c5a87d05ff12e92c9106efffc05bf8c4a1bf10de744bed5dc202fbca427b2f8/download"))()
     end
 end)
