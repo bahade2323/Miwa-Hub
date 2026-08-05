6b6ad8f0315d85d833711f528aaa5ab0c023569f
bahade2323 <bahade2323@gmail.com>
2026-08-05 13:36:55 +0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -2,8 +2,6 @@ task.spawn(function()
     local GameId = game.GameId
     if GameId == 10093833731 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/bahade2323/Miwa-Hub/refs/heads/main/Broken%20Blade"))()
-    elseif GameId == 10267363348 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/bahade2323/Miwa-Hub/refs/heads/main/Drain%20A%20Lake"))()
     elseif GameId == 10178802449 then
         loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/1c5a87d05ff12e92c9106efffc05bf8c4a1bf10de744bed5dc202fbca427b2f8/download"))()
     end
