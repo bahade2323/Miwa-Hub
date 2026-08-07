58b9e27f94982d85c3bf11dc500862a4f567a285
bahade2323 <bahade2323@gmail.com>
2026-08-08 03:35:49 +0800

    Update script loading based on GameId

--- Main.lua
+++ Main.lua
@@ -1,7 +1,7 @@
 task.spawn(function()
     local GameId = game.GameId
     if GameId == 10093833731 then
-        loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/234d0ecda12a85ccf8b2ece57e185dc7f0ffe1bee5a9be3163d371713c1ff84e/download"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/bahade2323/Miwa-Hub/refs/heads/main/BrokenBlade.lua"))()
     elseif GameId == 10178802449 then
         loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/1c5a87d05ff12e92c9106efffc05bf8c4a1bf10de744bed5dc202fbca427b2f8/download"))()
     end
